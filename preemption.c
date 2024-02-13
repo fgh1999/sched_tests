85b52a28a7ebc0622329a9cacc081195ccdf8d34
fgh1999 <fgh@stu.pku.edu.cn>
2024-02-13 23:25:25 +0800

    Add child end time validation

--- preemption.c
+++ preemption.c
@@ -13,11 +13,17 @@ typedef void (*workload_t)(void);
 typedef struct worker_t
 {
     int pid;
-    int nice;
+    int nice;           // normal priority value
     workload_t workload;
-    time_t start_time;
-    time_t end_time;
+    time_t start_time;  // The time when the workload is started.
+    time_t end_time;    // The time when the workload finishes.
 } worker_t;
+
+typedef struct worker_collection_t
+{
+    worker_t **workers;
+    int count;
+} worker_collection_t;
 
 worker_t *fork_normal_worker(workload_t workload, int nice)
 {
@@ -47,30 +53,52 @@ void wait_worker(worker_t *worker)
 {
     int status;
     waitpid(worker->pid, &status, 0);
-    if (WIFEXITED(status))
-    {
-        log("Worker %d exited with status %d\n", worker->pid, WEXITSTATUS(status));
-    }
-    else
-    {
+    if (!WIFEXITED(status))
         log("Worker %d exited abnormally\n", worker->pid);
+}
+
+void wait_workers(const worker_collection_t collection)
+{
+    int count = collection.count;
+    while (count-- > 0)
+    {
+        wait_worker(collection.workers[count]);
     }
 }
 
-void wait_workers(worker_t **workers, int count)
+void free_workers(const worker_collection_t collection)
 {
-    while (count-- > 0)
+    for (int i = 0; i < collection.count; i++)
     {
-        wait_worker(workers[count]);
+        free(collection.workers[i]);
     }
 }
 
-void free_workers(worker_t **workers, int count)
+/**
+ * @brief Compare two workers by their nice values ascendingly.
+ */
+int cmp_worker_by_nice(const void *a, const void *b)
 {
-    while (count-- > 0)
+    return ((worker_t *)a)->nice - ((worker_t *)b)->nice;
+}
+
+/**
+ * @brief Check the workers' end times according to their nice values.
+ *
+ * @param workers The workers to be checked.
+ * @return 1 if the all workers' end time are valid , 0 otherwise.
+ */
+int valid_end_time_order(worker_collection_t workers)
+{
+    qsort(workers.workers, workers.count, sizeof(worker_t *), cmp_worker_by_nice);
+    for (int i = 0; i < workers.count - 1; i++)
     {
-        free(workers[count]);
+        if (difftime(workers.workers[i]->end_time, workers.workers[i + 1]->end_time) > 0)
+        {
+            return 0;
+        }
     }
+    return 1;
 }
 
 void io_bound_workload()
@@ -78,23 +106,35 @@ void io_bound_workload()
     sleep_for(5, 1);
 }
 
-void io_bound_test(const int worker_count, const int *nice_targets)
+/**
+ * @brief Test the workers' behavior under different nice values.
+ *
+ * @param worker_count The number of workers to be tested.
+ * @param nice_targets The target nice values of the workers.
+ * @return 1 if the all workers' end time are valid , 0 otherwise.
+ */
+int io_bound_test(const int worker_count, const int *nice_targets)
 {
-    worker_t **workers = malloc(sizeof(worker_t *) * worker_count);
+    worker_collection_t worker_collection = {
+        .workers = malloc(sizeof(worker_t *) * worker_count),
+        .count = worker_count,
+    };
 
-    for (int i = 0; i < worker_count; i++)
-    {
-        workers[i] = fork_normal_worker(io_bound_workload, nice_targets[i]);
-    }
+    for (int i = 0; i < worker_collection.count; i++)
+        worker_collection.workers[i] = fork_normal_worker(io_bound_workload, nice_targets[i]);
 
-    wait_workers(workers, worker_count);
-    free_workers(workers, worker_count);
+    wait_workers(worker_collection);
+    int ret = valid_end_time_order(worker_collection);
+    if (ret == 0)
+        log("Some workers' end time are invalid\n");
+    free_workers(worker_collection);
+    return ret;
 }
 
 int main()
 {
-
-    io_bound_test(2, (int[]){1,2});
+    if (!io_bound_test(2, (int[]){1, 2}))
+        log("io_bound_test failed\n");
     // 工作负载：
     // 计算的话比如素因子分解，IO就用“中间包含间断的sleep”模拟一下吧
     // 可以多试几种工作负载
